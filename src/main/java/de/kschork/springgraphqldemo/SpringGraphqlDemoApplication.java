a8df2e512c4fab04c35ba7cd904ee2623708cb1a
kevinschork <kevinschork@live.de>
2022-05-05 10:50:00 +0200

    Initial commit

---
+++ src/main/java/de/kschork/springgraphqldemo/SpringGraphqlDemoApplication.java
@@ -0,0 +1,13 @@
+package de.kschork.springgraphqldemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGraphqlDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGraphqlDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/kschork/springgraphqldemo/SpringGraphqlDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package de.kschork.springgraphqldemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGraphqlDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
