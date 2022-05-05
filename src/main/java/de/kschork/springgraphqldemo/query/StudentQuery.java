ca302a38aab5df14fa98ff7dbdc69e332a9756f0
kevin.schork <kevin.schork@kuehne-nagel.com>
2022-05-05 18:32:57 +0200

    Add graphQL dependencies

---
+++ src/main/java/de/kschork/springgraphqldemo/query/StudentQuery.java
@@ -0,0 +1,9 @@
+package de.kschork.springgraphqldemo.query;
+
+import com.coxautodev.graphql.tools.GraphQLQueryResolver;
+import org.springframework.stereotype.Component;
+
+@Component
+public class StudentQuery implements GraphQLQueryResolver {
+
+}
